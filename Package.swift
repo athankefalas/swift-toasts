2497361e8132ae7875a595fb5547c814421392e9
Athanasios Kefalas <sakiskefalas@yahoo.gr>
2026-07-27 21:29:17 +0300

    Disabled previews flag.

--- Package.swift
+++ Package.swift
@@ -24,7 +24,7 @@ let package = Package(
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "SwiftToasts",
-            swiftSettings: [.define("ENABLE_PREVIEWS")]
+            swiftSettings: []
         ),
         .testTarget(
             name: "SwiftToastsTests",
